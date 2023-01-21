043b8f221643316e0456f1e147047d2dd3795a37
BlizzardDragon <blizzarddragon@list.ru>
2023-01-21 16:42:35 +0300

    Light bugs

--- Assets/Scripts/SceneOperator.cs
+++ Assets/Scripts/SceneOperator.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 
 public class SceneOperator : MonoBehaviour
 {
-    public static SceneOperator Instance; 
+    public static SceneOperator Instance;
     [SerializeField] Text _epilepsy;
     float _timer;
 
     void Awake()
     {
 
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
@@ -26,11 +26,11 @@ public class SceneOperator : MonoBehaviour
 
     void Update()
     {
-        if(Input.anyKey)
+        if (Input.anyKey)
         {
-            if(SceneManager.sceneCountInBuildSettings - 1 == SceneManager.GetActiveScene().buildIndex)
+            if (SceneManager.sceneCountInBuildSettings - 1 == SceneManager.GetActiveScene().buildIndex)
             {
-                Debug.Log(SceneManager.sceneCountInBuildSettings + " "+ SceneManager.GetActiveScene().buildIndex);
+                Debug.Log(SceneManager.sceneCountInBuildSettings + " " + SceneManager.GetActiveScene().buildIndex);
                 LoadScene(0);
             }
             else
@@ -38,8 +38,6 @@ public class SceneOperator : MonoBehaviour
                 LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
-
-        //ChangeColorText();
     }
 
     public void ReadStringInput(string S)
@@ -51,39 +49,5 @@ public class SceneOperator : MonoBehaviour
     void LoadScene(int index)
     {
         SceneManager.LoadScene(index);
-    }
-
-    private void ChangeColorText()
-    {
-        _timer += + Time.deltaTime * 1;
-        
-        if (_timer < 1)
-        {
-            _epilepsy.color = Color.red;
-        }
-
-        else if (_timer < 2)
-        {
-            _epilepsy.color = Color.blue;
-        }
-
-        else if (_timer < 3)
-        {
-            _epilepsy.color = Color.yellow;
-        }
-
-        else if (_timer < 4)
-        {
-            _epilepsy.color = Color.magenta;
-        }
-        
-        else if (_timer < 5)
-        {
-            _epilepsy.color = Color.green;
-        }
-        else
-        {
-            _timer = 0;
-        }
     }
 }
